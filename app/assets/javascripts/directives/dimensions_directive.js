c6e35d05d198420500664dbff79148798015788c
Alexandre Gonçalves <alexandre.dgoncalves@gmail.com>
2014-02-19 15:54:55 +0000

    fixed context update on resize

--- app/assets/javascripts/directives/dimensions_directive.js
+++ app/assets/javascripts/directives/dimensions_directive.js
@@ -1,12 +1,14 @@
 var app = angular.module("spacialAnalysis");
 
-app.directive('dimensions', function($window, ContextService) {
+app.directive('dimensions', function($window, ContextService, $rootScope) {
   return function (scope, element) {
     angular.element($window).bind('resize', function () {
       var map = document.getElementById("map_canvas");
       ContextService.map.view.canvasDimensions.width  = map.clientWidth;
       ContextService.map.view.canvasDimensions.height = map.clientHeight;
+      $rootScope.$apply();
       console.log(ContextService);
+      window.context = ContextService;
     });
   };
 });
