6d4d2119a9494571f0e178e86c6e22fd34074570
joaocampinhos <joao.campinhos@gmail.com>
2014-02-25 12:34:57 +0000

    charts icon

--- app/assets/javascripts/controllers/float_canvas_controller.js
+++ app/assets/javascripts/controllers/float_canvas_controller.js
@@ -2,24 +2,37 @@ var app = angular.module("spacialAnalysis");
 
 app.controller("floatCanvasController", function($scope) {
 
+  $scope.showPnl = false;
+  $scope.showBtn = false;
+  $scope.$on('selected', function() {
+    $scope.showPnl = false;
+    $scope.showBtn = false;
+  });
+
+  $scope.toggle = function() {
+    $scope.showPnl = !$scope.showPnl;
+    $scope.showBtn = !$scope.showBtn;
+  };
+
   $scope.$on('statisticLoaded', function(event, result){
-    console.log('Chart Draw')
-    $scope.template = '/charts/'+result.type+'.html'
-    $scope.chart = nvd_format_value_stats(result.stats)
-  })
+    console.log('Chart Draw');
+    $scope.showBtn = true;
+    $scope.template = '/charts/'+result.type+'.html';
+    $scope.chart = nvd_format_value_stats(result.stats);
+  });
 
   $scope.yAxisThickFormat = function(){
     return function(val){
       return val.toFixed(3);
-    }
-  }
+    };
+  };
 
   function format_value_stats(stats){
-    var labels = _.sortBy(_.keys(stats), function(label){return parseFloat(label)});
-    var values = []
+    var labels = _.sortBy(_.keys(stats), function(label){return parseFloat(label);});
+    var values = [];
     _.each(labels, function(label){
-      values.push(+stats[label].toFixed(5))
-    })
+      values.push(+stats[label].toFixed(5));
+    });
     return { labels: labels, datasets: [ { fillColor : "#a5a7a8", data: values } ] };
   }
 
@@ -28,7 +41,7 @@ app.controller("floatCanvasController", function($scope) {
   }
 
   function nvd_format_value_stats(stats){
-    var labels = _.sortBy(_.keys(stats), function(label){return parseFloat(label)});
+    var labels = _.sortBy(_.keys(stats), function(label){return parseFloat(label);});
     var values = _.map(labels, function(label){
       return [parseFloat(label), stats[label]];
     });
